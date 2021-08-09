322312d58a71a80bb2403a86c14ab6c9bc3eeecb
Ben <benjx32@gmail.com>
2021-08-09 13:57:28 -0700

    setting stuff up

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import java.util.ArrayList;
+
+public class Main{
+    boolean gameActive = true;
+    JFrame board = new JFrame("Snake");
+    int boardsize = 600;
+    
+    
+
+    public Main(){
+        board.setSize(boardsize,boardsize);
+        board.setVisible(true);
+        board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void tick(){
+
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+    }
+}
---
+++ src/food.java
@@ -0,0 +1,3 @@
+public class food {
+    int fweight = 5;
+}
